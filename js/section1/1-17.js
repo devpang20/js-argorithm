5f91b5d5ef547b090254fc3c029b6afc197941c5
sik2 <devpang20@gmail.com>
2021-10-27 06:19:18 +0900

    feat:1-17 중복단어 제거

---
+++ js/section1/1-17.js
@@ -0,0 +1,10 @@
+function soluation(str) {
+  const arr = str.split(",");
+
+  //갓성비... Set 키워드
+  return [...new Set(arr)].join("");
+}
+
+const arr = ["good", "time", "good", "time", "student"];
+
+soluation("ksekkset");
